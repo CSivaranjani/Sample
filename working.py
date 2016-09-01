501db1336afb5a696489826aee49e43fd5df0867
CSivaranjani <csivaranjani95@gmail.com>
2016-09-02 00:27:55 +0530

    Create working.py

---
+++ working.py
@@ -0,0 +1,7 @@
+s=input()
+if s=='Sunday' or s=='sunday' or s=='SUNDAY':
+  print 'false'
+else if s=='Monday' or s=='Tuesday' or s=='Wednesday' or s=='Thursady' or s=='Friday' or s=='Saturday' or s=='monday' or s=='tuesday' or s=='wednesday' or s=='thursady' or s=='friday' or s=='saturday':
+  print 'true'
+else:
+  print 'invalid day'
